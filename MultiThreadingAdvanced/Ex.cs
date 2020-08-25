999ed3beaf10ab2b2d5c94ad6a2a4da5df61b730
thelittlecitizen <thelittlecitizen7@gmail.com>
2020-08-25 01:17:43 -0700

    finish

---
+++ MultiThreadingAdvanced/Ex.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultiThreadingAdvanced
+{
+    public class Ex
+    {
+        public Ex()
+        {
+        }
+
+        // 1
+        public IEnumerable<int> GetNumbers(IEnumerable<int> numbers)
+        {
+            return numbers.AsParallel().Where(n => n % 3 == 1).ToList();
+        }
+
+        // 2
+        public async Task WriteTextAsync(string messsage)
+        {
+            string path = @"C:\Empire.txt";
+            using (StreamWriter streamWriter = new StreamWriter(path))
+            {
+                await streamWriter.WriteLineAsync(messsage);
+            }
+
+        }
+
+
+    }
+
+}
---
+++ MultiThreadingAdvanced/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace MultiThreadingAdvanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ex ex1 = new Ex();
+
+            ex1.WriteTextAsync("Mamas").Wait();
+        }
+
+    }
+}
